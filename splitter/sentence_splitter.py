add4546de046f4a13ede3bf8e0bc48e038888bff
Existentio <florentist@yandex.ru>
2020-12-06 01:03:55 +0300

    first version

--- splitter/sentence_splitter.py
+++ splitter/sentence_splitter.py
@@ -3,9 +3,8 @@ Split sentences of your pseudo requirements for further processing
 
 """
 
-
 # list of simple numbers [convert list of int to list of str]
-simple_numbers = [str(x) for x in list(range(0, 10))]
+simple_numbers = [str(x) for x in list(range(0, 100))]
 
 # punctuation marks
 punctuation_marks = [',', '.', ':', ';']
@@ -13,19 +12,17 @@ punctuation_marks = [',', '.', ':', ';']
 #
 starters = ['Мр.']
 
-# provide a label for element of sentence
-label_req_id_start = 'req_id_start'
-label_req_id_end = 'req_id_end'
+# label for element of sentence
+label_req_id_start = 'REQ_ID_START'
+label_req_id_end = 'REQ_ID_END'
 label_req_main_sentence_start = ''
 label_req_main_sentence_end = ''
 
 # sentence with labels for further processing
 sentence_with_labels = ''
 
-
 # minimal potential length of requirement
 min_req_len = 8
-
 
 test_req_1 = 'При первом открытии приложения "Х" должен появляться новый экран. ' \
              '1.1 Если экран успешно открылся, то начнет загружаться новая страница. ' \
@@ -65,32 +62,34 @@ print(divide_req(test_req_1))
 
 print('===')
 
-req_sample = '1.2.3.4 you must do this 2.3'
+req_sample = '1.2.3.4 you must do - this 2.3'
 
 # todo need to add req identifiers to list with seperated value i.e. whitespace and slice it to str
 #  to make sure that req like "1.2 User can press the button and executes running 1.3" will be ok
 
 
 
-idx = -1
 
-sentence_with_labels += label_req_id_start
+def define_sentence_labels():
+    sentence_with_labels = label_req_id_start
+    idx = 0
 
-for symb in req_sample:
-    # if symb in simple_numbers:
-    # print(req_sample.index(symb))
+    for symb in req_sample:
+        if (req_sample[(req_sample.index(symb))]) in punctuation_marks or simple_numbers:
 
-    if (req_sample[(req_sample.index(symb) + idx)]) in punctuation_marks or simple_numbers:
-        if req_sample[(req_sample.index(symb))] == ' ':
-            sentence_with_labels+=label_req_id_end
-            break
-        else:
-            idx = (idx + 1)
-            print('here is punctuation mark or simple number, moving next')
-            print(symb)
-            sentence_with_labels+=(req_sample[(req_sample.index(symb))])
+            if req_sample[(req_sample.index(symb))] == ' ':
+                idx += 1
+                if idx == 1:
+                    sentence_with_labels += label_req_id_end
+                    print('printing symb' + symb)
+                else:
+                    sentence_with_labels += (req_sample[(req_sample.index(symb))])
+            else:
+                # idx = (idx + 1)
+                print('here is punctuation mark or simple number, moving next: ' + symb)
+                sentence_with_labels += (req_sample[(req_sample.index(symb))])
 
-# else:
-#     'bad algorithm'
+    return sentence_with_labels
 
-print(sentence_with_labels)
+
+print(define_sentence_labels())
