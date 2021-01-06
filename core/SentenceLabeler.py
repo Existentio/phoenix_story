8ab97864e7d4bc371b7664eb2bef2abc89829fe6
Existentio <florentist@yandex.ru>
2021-01-07 01:35:55 +0300

    sentence labeler refactoring

--- core/SentenceLabeler.py
+++ core/SentenceLabeler.py
@@ -1,10 +1,10 @@
-
-
 # req_sample = '1. According to 1.2.3.4 system must .. do it 20 times,  2.3.3.5.6.643 and it should take <= 0.5 seconds.'
+from core.data.StopWordsManager import StopWordsManager
+
 req_sample = '1. Я думаю , согласно п. 1.2.3.4 система иногда должна .. выполнив операцию 20 раз, прочитав система быть ясно что 2.3.3.5.6.643 и это должно занять <= 0.5 секунд.'
 
 # list of simple numbers [convert list of int to list of sentence]
-simple_numbers = [str(x) for x in list(range(0, 100))]
+numbers = [str(x) for x in list(range(0, 100))]
 
 punctuation_marks = [',', '.', ':', ';']
 
@@ -15,44 +15,71 @@ label_req_id_start = 'REQ_ID_START'
 label_req_id_end = 'REQ_ID_END'
 label_req_main_sentence_start = 'MAIN_START'
 label_req_main_sentence_end = 'MAIN_END'
+label_uml_modifier = 'UML_MODIFIER'
 
+time_units = ['секунд', 'минут', 'час', 'день']
 
 class SentenceLabeler:
     """Labels sentence parts."""
 
-    # todo need to add req identifiers to list with seperated value i.e. whitespace and slice it to sentence
-    #  to make sure that req like "1.2 User can press the button and executes running 1.3" will be ok
 
     # todo need label sentence for separating req identifiers from words
 
-    # todo need take into account that req may be contain smth like this:
-    #  "operation should be take no more than 0.5 sec.."
+
+    def __init__(self):
+        self.swm = StopWordsManager()
 
     def define_sentence_labels(self):
         sentence_with_labels = ''
-        counter = 0
+        cnt_ext = 0
 
-        for symb in req_sample:
-            if (req_sample[(req_sample.index(symb))]) in punctuation_marks:
-                print('here is punctuation mark, moving next: ' + symb)
-                sentence_with_labels += (req_sample[(req_sample.index(symb))])
-
-            elif (req_sample[(req_sample.index(symb))]) in simple_numbers:
-                counter += 2
-                print('here is simple number, moving next: ' + symb)
-                sentence_with_labels += (req_sample[(req_sample.index(symb))])
-            elif (req_sample[(req_sample.index(symb))]) == ' ':
-                if counter >= 2:
-                    sentence_with_labels += label_req_id_end
-                    counter = 0
-                sentence_with_labels += (req_sample[(req_sample.index(symb))])
-                print('here is whitespace')
-
+        for x in range(len(req_sample.split())):
+            for y in req_sample.split()[x]:
+                if y == '.':
+                    cnt_ext += 1
+            try:
+                if cnt_ext == 1:
+                    if (req_sample.split()[x + 1] in time_units) \
+                            or (req_sample.split()[x + 1][:len(req_sample.split()[x + 1]) - 1] in time_units):
+                        print('it is time unit: ', req_sample.split()[x + 1])
+                        sentence_with_labels += req_sample.split()[x] + ' '
+                    else:
+                        print('it is req id', req_sample.split()[x])
+                        sentence_with_labels += label_req_id_end + ' '
+                    cnt_ext = 0
+            except IndexError:
+                print('got index error', req_sample.split()[x])
+                pass
+            if len(req_sample.split()[x]) == 1 and req_sample.split()[x] == '.':
+                print('it is an end sentence dot: ', req_sample.split()[x])
+            if cnt_ext == 2:
+                cnt_int = 0
+                for y in range(len(req_sample.split()[x])):
+                    if req_sample.split()[x][y] in numbers:
+                        cnt_int += 1
+                    if cnt_int < 4 and req_sample.split()[x][y] == '.':
+                        cnt_int += 1
+                    if cnt_int == 4 and req_sample.split()[x][y] in numbers:
+                        print('it is uml modifier: ', req_sample.split()[x])
+                        sentence_with_labels += req_sample.split()[x] + ' '
+                cnt_ext = 0
+            elif cnt_ext > 2:
+                cnt_int = 0
+                for y in range(len(req_sample.split()[x])):
+                    if req_sample.split()[x][y] in numbers:
+                        cnt_int += 1
+                    if cnt_int == 1 and req_sample.split()[x][y - 1] == '.':
+                        print('it is a typo: ', req_sample.split()[x])
+                        sentence_with_labels += req_sample.split()[x] + ' '
+                        break
+                    if cnt_int == 1:
+                        print('it is a req id: ', req_sample.split()[x])
+                        sentence_with_labels += label_req_id_end + ' '
+                        break
+                    cnt_ext = 0
             else:
-                print('here is sentence character, moving next: ' + symb)
-                sentence_with_labels += (req_sample[(req_sample.index(symb))])
-
-        return sentence_with_labels.strip()
+                sentence_with_labels += req_sample.split()[x] + ' '
+        return sentence_with_labels
 
     def bind_sentence_words_with_ids(self):
         dict_labeled_str = {}
@@ -78,3 +105,11 @@ class SentenceLabeler:
 
         print(main_sentence_without_req_id)
         return main_sentence_without_req_id
+
+
+sl = SentenceLabeler()
+
+print(sl.define_sentence_labels())
+
+print('====\n')
+sl.detach_main_sentence()
